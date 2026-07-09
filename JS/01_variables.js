1516fbf92c4f5b0e4c4892dc120b51cfa38ee6a2
baalahuvro <balkrishnverma26@gmail.com>
2026-07-09 19:07:43 +0000

    datatypes

---
+++ JS/01_variables.js
@@ -0,0 +1,8 @@
+const acccountId=12345
+let accountEmail="baala@gmail.com"
+accountCity="dhanbad"
+// accountID=2 not allowed
+// use of var is used now bcz of issue in block code and functional code
+accountEmail="hehe@gmail.com"
+accountCity="mumbai"
+console.table([accountEmail,accountCity,acccountId])
---
+++ JS/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";// new version k use
+
+console.log(3+3) 
+console.log("baala")
+let age=21
+let name="Baala"
+let ispresent=false
+// bigint string boolean null number=> 2 ka power 53
+// undefined=>
+// symbol=>
+    console.log(typeof "Baala")

3c74b7365563f7d60c4ab999baf5bf7f33b728e7
baalahuvro <balkrishnverma26@gmail.com>
2026-07-10 18:10:26 +0000

    stack aur heap krliye

---
+++ JS/stackndheap.js
@@ -0,0 +1,17 @@
+// stack-primitive data type mtlb isme copy HEAP-non primitive data type mtlb direct change 
+let myutubename= "baaladotcom"
+let anothername= myutubename
+anothername="baala"
+
+console.log(anothername);
+console.log(myutubename);
+let userone = {
+    email: "user@gmail.com",
+    uppi: "user@ybl"
+}
+let usertwo= userone
+usertwo.email="baala@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
+
+

29787798901b2b393c91b82117c7c2124a7ba792
baalahuvro <balkrishnverma26@gmail.com>
2026-07-21 19:53:26 +0530

    learned scopes

---
+++ JS/03_basics/02_scopes.js
@@ -0,0 +1,43 @@
+
+if(true){
+    let a=10
+    const b=20
+    var c=30
+
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one(){
+//     const username ="baala"
+    
+//     function two(){
+//         const website="youtube"
+//         console.log(username);   
+//     }
+//     console.log(website);
+//     two()
+    
+// }
+// one()
+// if(true){
+//     const username="baalahu"
+//     if(username==="baalahu"){
+//         const website ="youtube"
+//         console.log(username+website);
+//     }
+//     console.log(website);
+// }
+// console.log(username);
+//+++++++++ interesting
+
+console.log(addone(5))
+function addone(num){
+    return num + 1 
+}
+
+addtwo(5)
+const addtwo =function(num){
+    return num +2
+}

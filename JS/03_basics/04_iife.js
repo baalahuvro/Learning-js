15b1e328ceab63e5a0f07dde0c4ef6c650afdf61
baalahuvro <balkrishnverma26@gmail.com>
2026-07-21 20:24:59 +0530

    leanred iife

---
+++ JS/03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// immediately invoked function expressions--- global scope ke pollution se dikkt hoti hai isliye we use it
+(function chai(){
+    //named iife
+    console.log("db connected");
+    
+})(); // do iife likhne ke liye this semicolong is necessary
+// console.log(chai())
+((name) => {
+    console.log("DB Connected Two");
+})("baala");

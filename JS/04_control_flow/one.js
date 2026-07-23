7d5ef0ad5a6ed5d26f6699cb80c86043b2307fa9
baalahuvro <balkrishnverma26@gmail.com>
2026-07-23 18:16:21 +0530

    if else learned

---
+++ JS/04_control_flow/one.js
@@ -0,0 +1,46 @@
+// const isuserloggedin = true
+// const temprature =41
+// if(temprature<50){
+//     console.log("less than 50");
+    
+// }else{
+//     console.log("temprature is grater than 50");
+    
+// }
+// console.log("temprature is greater than 50");
+
+// const score =200
+// if(score>100){
+//     const power ="fly"
+//     console.log(`user power-${power}`);
+    
+// }
+//
+
+
+const balance = 1000;
+// if(balance>500) console.log("test"),
+// console.log("dont use aese comma deke");
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else if (balance < 1250) { // Fixed: Added 'if' here
+//     console.log("less than 1250");
+// } else {
+//     console.log("1250 or greater");
+// }
+const userloggedin= true
+const debitcard = true
+const loggedinfromgoogle=true
+const loggedinfromemail=true
+if(userloggedin && debitcard){
+    console.log('allow to buy course');
+}
+if(loggedinfromgoogle || loggedinfromemail){
+    console.log("user logged in");
+
+}
+
